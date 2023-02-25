5f064f07428fbd3e892c89ed41bc5c7f632cd58f
NathanAud <nathaniel.odi@gmail.com>
2023-02-25 20:28:39 +0700

    Add test file

---
+++ Tugas Python/fake.py
@@ -0,0 +1,9 @@
+print(
+    """Enter a number: abc
+Error: 'abc' is not a valid number. Please try again.
+Enter a number: 5
+Enter a number: 10
+Enter a number: 2
+Enter a number: 0
+The sum of the squares of the even numbers is: 104"""
+)
