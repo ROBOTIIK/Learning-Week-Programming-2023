a90863579ba96b09bc11130211a768a597f14af2
Fawwaz Anrico <fawwazanricopurnomo@gmail.com>
2023-02-26 08:51:09 +0700

    Added a new folder to my branch

---
+++ Task1/soal2.py
@@ -0,0 +1,28 @@
+## Fawwaz Anrico Purnomo 215150301111019
+class Person:
+    def __init__(self, name, id_number):
+        self.name = name
+        self.id_number = id_number
+
+
+class Student(Person):
+    def __init__(self, name, id_number, courses):
+        super().__init__(name, id_number)
+
+        self.courses = courses
+
+class Professor(Person):
+    def __init__(self, name, id_number, courses_taught):
+        super().__init__(name, id_number)
+
+        self.courses_taught = courses_taught
+
+
+def main():
+    s = Student("Alice", 1234, ["Introduction to Computer Science", "Data Structures"])
+    p = Professor("Bob", 5678, ["Database Systems", "Web Development"])
+    print(s.name, s.id_number, s.courses)
+    print(p.name, p.id_number, p.courses_taught)
+
+if __name__ == "__main__":
+    main()
